788e910489b082f159431e076fc0bbc222cca45c
Ethem Cem Özkan <me@ethemcemozkan.com>
2025-03-24 22:14:28 +0100

    V2 remove big logo

--- src/Home.js
+++ src/Home.js
@@ -16,29 +16,21 @@ function Home() {
         <div className="container-fluid nav_bg">
           <div className="row">
             <div className="col-10 mx-auto">
-              <div className="row">
-                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+              <div className="row pt-5">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column description">
                   <h1>
                     Grow your bussiness with
                     <strong className="brand-name"> Celebron IT Solutions. </strong>
                   </h1>
 
                   <h5 className="my-3">
-                  Specializing in DevOps consulting, we help businesses design, optimize, and modernize their AWS, Azure, and on-premises environments. Our expertise includes cloud migration, infrastructure automation, CI/CD, observability, and cloud modernizations, ensuring secure, efficient, and high-performing systems. With deep knowledge of cloud and hybrid architectures, we enable organizations to streamline operations and drive innovation.
+                    Specialising in DevOps consulting, we help businesses design, optimise, and modernise their AWS, Azure, and on-premises environments. Our expertise includes cloud implementation, infrastructure automation, containerisation, cloud migration, CI/CD, observability, and cloud modernisation — ensuring secure, efficient, and high-performing systems. With deep knowledge of cloud and hybrid architectures, we enable organisations to streamline operations, adopt container technologies, leverage the public cloud, and manage costs effectively.
                   </h5>
-                </div>
-
-                <div className="col-lg-6 order-1 order-lg-2 header-img">
-                  <img
-                    src={web}
-                    className="img-fluid"
-                    alt="HomeImg"
-                  />
                 </div>
               </div>
             </div>
           </div>
-          <div className="row">
+          <div className="row pt-5">
             <div>
               <h1 className="text-center">Our Services</h1>
             </div>
